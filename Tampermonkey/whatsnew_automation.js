39e9a3a1e97dca62d89d99657356dc10e23a5f8f
noenemy <noenemy@gmail.com>
2021-07-04 23:25:39 +0900

    Implement file download feature

--- Tampermonkey/whatsnew_automation.js
+++ Tampermonkey/whatsnew_automation.js
@@ -5,8 +5,7 @@
 // @description  try to take over the world!
 // @author       SoonKeun Kim (@noenemy)
 // @include      /^https?://aws\.amazon\.com/about-aws/whats-new/\d{4}\/\d{2}\/[a-z]+
-// @require      http://code.jquery.com/jquery-latest.js
-// @grant        none
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 var LOCAL_STORAGE_NAME = "whatsnew_automation_localstorage";
@@ -57,6 +56,54 @@ function isUrlInCart(url) {
     return false;
 }
 
+function checkOutCart() {
+    var array = JSON.parse(localStorage.getItem(LOCAL_STORAGE_NAME));
+    if (array === null) {
+        console.log("local Storge not found.");
+        return false;
+    }
+    //console.log(array);
+
+    array.forEach(item => {
+        console.log(item);
+    });
+
+    var body = new Object();
+    body.url_list = array;
+    console.log(JSON.stringify(body));
+
+    GM_xmlhttpRequest({
+        method: "POST",
+        url: "https://cg83wv7uce.execute-api.ap-northeast-2.amazonaws.com/Prod/whatsnew",
+        data: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        },
+        onload: function(result) {
+            if (this.status == 200) {
+                console.log("200 OK");
+                var json = JSON.parse(result.response);
+                console.log(json)
+                var body = JSON.parse(json.body);
+                var downloadUrl = body.url;
+                console.log(downloadUrl)
+                downloadURI(downloadUrl, "test.pptx");
+            }
+            //var json = $.parseJSON(response);
+        }
+    });
+}
+
+function downloadURI(uri, name) {
+  var link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  delete link;
+}
+
 function showAddOrRemoveCartItemButton() {
 
     var element = document.getElementById('whatsnew_button__addtocart');
@@ -88,7 +135,7 @@ function showWhatsNewAutomationButtons() {
     if (localStorageEnabled === true) {
     span.innerHTML +=
         ' <a id="whatsnew_button__addtocart" href="#">[Add to Cart]</a>' +
-        ' <a id="whatsnew_button__cart" onclick="checkOut()" href="#">[Cart(0)]</a>';
+        ' <a id="whatsnew_button__cart" href="#">[Cart(0)]</a>';
     }
 
     // Attach to the page
@@ -100,6 +147,14 @@ function showWhatsNewAutomationButtons() {
     if (addtocart_link) {
         console.log("found");
         addtocart_link.addEventListener("click", addOrRemoveWhatsNewItemToCart);
+    } else {
+        console.log("not found");
+    }
+
+    var cart_link = document.getElementById('whatsnew_button__cart');
+    if (cart_link) {
+        console.log("found");
+        cart_link.addEventListener("click", checkOutCart);
     } else {
         console.log("not found");
     }
